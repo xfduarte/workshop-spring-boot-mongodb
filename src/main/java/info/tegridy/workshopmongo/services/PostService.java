1bd0368c71f5c6f458c21fb6bb5670e0d611ac89
Fernando Duarte <duarte.brusque@gmail.com>
2020-06-17 00:58:26 -0300

    Obtendo um post por id

---
+++ src/main/java/info/tegridy/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package info.tegridy.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import info.tegridy.workshopmongo.domain.Post;
+import info.tegridy.workshopmongo.repositories.PostRepository;
+import info.tegridy.workshopmongo.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
