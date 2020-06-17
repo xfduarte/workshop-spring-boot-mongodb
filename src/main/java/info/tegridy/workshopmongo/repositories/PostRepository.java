84747ec9c1252478e18b9346900f7eca08219062
Fernando Duarte <duarte.brusque@gmail.com>
2020-06-17 13:59:32 -0300

    Consulta simples com @Query

--- src/main/java/info/tegridy/workshopmongo/repositories/PostRepository.java
+++ src/main/java/info/tegridy/workshopmongo/repositories/PostRepository.java
@@ -3,6 +3,7 @@ package info.tegridy.workshopmongo.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import info.tegridy.workshopmongo.domain.Post;
@@ -10,5 +11,8 @@ import info.tegridy.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
